86fa50a781eade49c6f42010fd83f14e3ed39d33
Victor Serrano <victor.serrano@unl.edu.ec>
2019-07-12 09:36:09 -0500

    años
    Aumento de dias del año

--- Simulacion_Bañeras/src/simulacion_bañeras/Simulacion_Bañeras.java
+++ Simulacion_Bañeras/src/simulacion_bañeras/Simulacion_Bañeras.java
@@ -22,45 +22,56 @@ public class Simulacion_Bañeras {
         int numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de Corridas"));
         double resultado = 0, suma = 0;
         int excedio = 0, noexcedio = 0;
+        double precioA = 0, precioAnual = 0, Xn = 1;
         DecimalFormat formato = new DecimalFormat("0.00000");
         System.out.println("+---------------+-----+-------------------------+-----------------------+-----------------------+---------+");
-        System.out.println("| Corridas      |Tina |      N. Aleatorio       |    Peso de la Tina    |     Peso acumulado    |Excedio? |");
+        System.out.println("| Años      |Tina |      N. Aleatorio       |    Peso de la Tina    |     Peso acumulado    |Excedio? |");
         System.out.println("+---------------+-----+-------------------------+-----------------------+-----------------------+---------+");
-
         for (int i = 0; i < numero; i++) {
-            suma = 0;
-            resultado = 0;
-            for (int j = 0; j < 5; j++) {
-                double random = Math.random() * 1;
-                if (random <= 0.5) {
-                    resultado = Math.sqrt(800 * random) + 190;
-
-                } else {
-                    resultado = 230 - Math.sqrt((1 - random) * 800);
-                }
-                suma = suma + resultado;
-
-                if (j == 4) {
-                    if (suma > 1000) {
-                        System.out.println("|\t \t|  " + (j + 1) + "  |\t\t" + formato.format(random) + "\t\t|\t" + formato.format(resultado) + "\t|\t" + formato.format(suma) + "\t|    " + "SI" + "   |");
-                        excedio++;
-                    } else {
-                        System.out.println("|\t \t|  " + (j + 1) + "  |\t\t" + formato.format(random) + "\t\t|\t" + formato.format(resultado) + "\t|\t" + formato.format(suma) + "\t|    " + "NO" + "   |");
-                        noexcedio++;
-                    }
-                    System.out.println("+---------------+-----+-------------------------+-----------------------+-----------------------+---------+");
-                } else {
-                    if (j == 0) {
-                        System.out.println("|\t" + (i + 1) + "\t|  " + (j + 1) + "  |\t\t" + formato.format(random) + "\t\t|\t" + formato.format(resultado) + "\t|\t" + formato.format(suma) + "\t|    " + "-" + "    |");
-                    } else {
-                        System.out.println("|\t \t|  " + (j + 1) + "  |\t\t" + formato.format(random) + "\t\t|\t" + formato.format(resultado) + "\t|\t" + formato.format(suma) + "\t|    " + "-" + "    |");
-                    }
-
-                }
+            precioA = 0;
+            for (int j = 0; j < 260; j++) {
+                suma = 0;
                 resultado = 0;
+                for (int k = 0; k < 5; k++) {
+                    Xn = (69069 * Xn) % (Math.pow(2, 37));
+                    double random = Xn / (Math.pow(2, 37));
+                    if (random <= 0.5) {
+                        resultado = Math.sqrt(800 * random) + 190;
+
+                    } else {
+                        resultado = 230 - Math.sqrt((1 - random) * 800);
+                    }
+                    suma = suma + resultado;
+
+                    if (k == 4) {
+                        if (suma > 1000) {
+                            precioA = precioA + 200;
+                            System.out.println("|\t \t|  " + (k + 1) + "  |\t\t" + formato.format(random) + "\t\t|\t" + formato.format(resultado) + "\t|\t" + formato.format(suma) + "\t|    " + "SI" + "   |");
+                            excedio++;
+
+                        } else {
+                            System.out.println("|\t \t|  " + (k + 1) + "  |\t\t" + formato.format(random) + "\t\t|\t" + formato.format(resultado) + "\t|\t" + formato.format(suma) + "\t|    " + "NO" + "   |");
+                            noexcedio++;
+                        }
+                        if (j==259) {
+                            System.out.println("Precio Adicional Anual: "+precioA);
+                        }
+                        System.out.println("+---------------+-----+-------------------------+-----------------------+-----------------------+---------+");
+                    } else {
+                        if (k == 0) {
+                            System.out.println("|\t" + (i + 1) + "\t|  " + (k + 1) + "  |\t\t" + formato.format(random) + "\t\t|\t" + formato.format(resultado) + "\t|\t" + formato.format(suma) + "\t|    " + "-" + "    |");
+                        } else {
+                            System.out.println("|\t \t|  " + (k + 1) + "  |\t\t" + formato.format(random) + "\t\t|\t" + formato.format(resultado) + "\t|\t" + formato.format(suma) + "\t|    " + "-" + "    |");
+                        }
+
+                    }
+                    resultado = 0;
+                }
             }
+            precioAnual = precioAnual + precioA;
         }
         System.out.println("Excedio el peso: " + excedio + "\nNo excedio el peso: " + noexcedio);
+        System.out.println("Precio Anual promedio en " + numero + " años: " + (precioAnual / numero));
 
     }
 
